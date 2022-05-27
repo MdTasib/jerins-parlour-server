cdd2f39534419312b6ce4e763dcd82b19b9c6fc6
MdTasib <ohidulalam442@gmail.com>
2022-05-27 22:21:44 +0600

    server

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.Payment_Secket_Key);
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Jerin Parlour server is open");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dryxz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const serviceCollection = client
+			.db("jerins-parlour")
+			.collection("services");
+		const bookingCollection = client.db("jerins-parlour").collection("booking");
+		const paymentCollection = client
+			.db("jerins-parlour")
+			.collection("payments");
+
+		// payment system
+		app.post("/create-payment-intent", async (req, res) => {
+			const service = req.body;
+			const price = service.price;
+			const amount = price * 100;
+			const paymentIntent = await stripe.paymentIntents.create({
+				amount: amount,
+				currency: "usd",
+				payment_method_types: ["card"],
+			});
+			res.send({ clientSecret: paymentIntent.client_secret });
+		});
+
+		// payment confirm
+		app.patch("/booking/:id", async (req, res) => {
+			const id = req.params.id;
+			const payment = req.body;
+			const filter = { _id: ObjectId(id) };
+			const updatedDoc = {
+				$set: {
+					productId: payment.productId,
+					status: payment.status,
+					name: payment.name,
+					description: payment.description,
+					img: payment.img,
+					paid: true,
+					transactionId: payment.transactionId,
+				},
+			};
+
+			const result = await paymentCollection.insertOne(payment);
+			const updatedBooking = await bookinCollection.updateOne(
+				filter,
+				updatedDoc
+			);
+			res.send(updatedDoc);
+		});
+
+		// get all services
+		app.get("/service", async (req, res) => {
+			const service = await serviceCollection.find().toArray();
+			res.send(service);
+		});
+
+		// get specific service
+		app.get("/service/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await serviceCollection.findOne(query);
+			res.send(result);
+		});
+
+		// booking a service
+		app.post("/booking", async (req, res) => {
+			const service = req.body;
+			const result = await bookingCollection.insertOne(service);
+			res.send(result);
+		});
+
+		// get booking on specific user
+		app.get("/booking/:email", async (req, res) => {
+			const email = req.params.email;
+			const filter = { userEmail: email };
+			const booking = await bookingCollection.find(filter).toArray();
+			res.send(booking);
+		});
+	} finally {
+	}
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => console.log("Jerin Parlour server is open"));
